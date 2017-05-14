76d499d0f208ed7ece493e330dfc779f297a1bcc
Zhijian <likely_zhao@163.com>
2017-05-14 17:24:16 +0800

    fix syntax error

--- example/rcnn/rcnn/dataset/imagenet.py
+++ example/rcnn/rcnn/dataset/imagenet.py
@@ -263,10 +263,10 @@ class imagenet(IMDB):
                 if len(dets) == 0:
                     continue
                         # the VOCdevkit expects 1-based indices
-                for k in range(dets.shape[0]):
-                #           f.write('{:s} {:.3f} {:.1f} {:.1f} {:.1f} {:.1f}\n'.
-                    format(index, dets[k, -1],
-                        dets[k, 0] + 1, dets[k, 1] + 1, dets[k, 2] + 1, dets[k, 3] + 1))
+                    for k in range(dets.shape[0]):
+#           f.write('{:s} {:.3f} {:.1f} {:.1f} {:.1f} {:.1f}\n'.
+                #    format(index, dets[k, -1],
+                #       dets[k, 0] + 1, dets[k, 1] + 1, dets[k, 2] + 1, dets[k, 3] + 1))
 
     def evaluate_detections_merge(self, detections_list):
         """
