4028c3fd65156d2720fbbf70e29e969d7b03f724
zjykzk <zjykzk@gmail.com>
2017-05-19 09:57:32 +0800

    fix train script

--- example/rcnn/fpn/train.sh
+++ example/rcnn/fpn/train.sh
@@ -8,5 +8,12 @@ export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
 export PYTHONUNBUFFERED=1
 
 nohup python train_end2end_fpn.py --network resnet_fpn                      \
-                                  --gpu 2,3                                 \
+                                  --dataset imagenet_loc_2017               \
+                                  --image_set train                         \
+                                  --root_path /disk2/data/imagenet_loc_2017 \
+                                  --dataset_path ILSVRC                     \
+                                  --prefix model/fpn_imagenet_loc_2017      \
+                                  --pretrained model/resnet-101             \
+                                  --pretrained_epoch 0                      \
+                                  --gpu 0                                   \
                                   >${LOG} 2>&1 &
